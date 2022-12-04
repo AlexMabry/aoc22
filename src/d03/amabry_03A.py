dca301e259ed950563f1af63d724b7d374a6fec5
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-04 09:45:54 -0600

    day3

--- src/d03/amabry_03A.py
+++ src/d03/amabry_03A.py
@@ -7,11 +7,14 @@ puzzle = models.Puzzle(year=2022, day=3)
 # format data
 input_data = parse_data(puzzle.input_data)
 
+
 ############################
 # Solve puzzle
-print(input_data)
+numbers = [[ord(c) - (96 if c.islower() else 38) for c in row] for row in input_data]
+compartments = [[set(n[:len(n)//2]), set(n[len(n)//2:])] for n in numbers]
+priorities = [set.intersection(*c).pop() for c in compartments]
 
-answer_to_submit = None
+answer_to_submit = sum(priorities)
 ############################
 
 # submit answer
--- src/d03/amabry_03B.py
+++ src/d03/amabry_03B.py
@@ -9,9 +9,11 @@ input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+numbers = [[ord(c) - (96 if c.islower() else 38) for c in row] for row in input_data]
+groups = [[set(numbers[n+r]) for r in range(3)] for n in range(0, len(numbers), 3)]
+badges = [set.intersection(*g).pop() for g in groups]
 
-answer_to_submit = None
+answer_to_submit = sum(badges)
 ############################
 
 # submit answer
