13ae963c6a65c347f400242238bad5b8ec6bd5d6
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-05 09:59:27 -0600

    day5

--- src/d05/amabry_05A.py
+++ src/d05/amabry_05A.py
@@ -1,3 +1,6 @@
+import re
+from collections import defaultdict, deque
+
 from aocd import models
 from src.utils import parse_data
 
@@ -9,9 +12,37 @@ input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
 
-answer_to_submit = None
+# find the top rows
+input_row = 0
+top = []
+while input_data[input_row]:
+    top.append(input_data[input_row])
+    input_row += 1
+
+# populate the stacks
+stacks = defaultdict(deque)
+for level in range(input_row - 2, -1, -1):
+    for col in range(1, 10):
+        if top[level][col*4-3] != ' ':
+            stacks[col].append(top[level][col * 4 - 3])
+
+# skip empty line
+input_row += 1
+
+# read the moves
+moves = []
+pattern = re.compile(r'move ([0-9]+) from ([1-9]) to ([1-9]+)')
+for r in range(input_row, len(input_data)):
+    moves.append([int(n) for n in pattern.search(input_data[r]).groups()])
+
+# perform the moves
+for num_to_move, from_stack, to_stack in moves:
+    for _ in range(num_to_move):
+        stacks[to_stack].append(stacks[from_stack].pop())
+
+# get answer
+answer_to_submit = ''.join(stack.pop() for col, stack in stacks.items())
 ############################
 
 # submit answer
--- src/d05/amabry_05B.py
+++ src/d05/amabry_05B.py
@@ -1,3 +1,6 @@
+import re
+from collections import defaultdict, deque
+
 from aocd import models
 from src.utils import parse_data
 
@@ -9,9 +12,36 @@ input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+# find the top rows
+input_row = 0
+top = []
+while input_data[input_row]:
+    top.append(input_data[input_row])
+    input_row += 1
 
-answer_to_submit = None
+# populate the stacks
+stacks = defaultdict(deque)
+for level in range(input_row - 2, -1, -1):
+    for col in range(1, 10):
+        if top[level][col*4-3] != ' ':
+            stacks[col].append(top[level][col * 4 - 3])
+
+# skip empty line
+input_row += 1
+
+# read the moves
+moves = []
+pattern = re.compile(r'move ([0-9]+) from ([1-9]) to ([1-9]+)')
+for r in range(input_row, len(input_data)):
+    moves.append([int(n) for n in pattern.search(input_data[r]).groups()])
+
+# perform the moves
+for num_to_move, from_stack, to_stack in moves:
+    to_move = deque([stacks[from_stack].pop() for _ in range(num_to_move)])
+    stacks[to_stack].extend(reversed(to_move))
+
+# get answer
+answer_to_submit = ''.join(stack.pop() for col, stack in stacks.items())
 ############################
 
 # submit answer
