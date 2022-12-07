8903ee0ac0e658180cea126e077eb561c59f42ad
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-07 14:37:51 -0600

    day7

--- src/d07/amabry_07B.py
+++ src/d07/amabry_07B.py
@@ -1,18 +1,45 @@
+import re
+from typing import Optional
+
 from aocd import models
 from src.utils import parse_data
+from dataclasses import dataclass, field
 
 # create puzzle
 puzzle = models.Puzzle(year=2022, day=7)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+@dataclass
+class Directory:
+    parent: Optional['Directory'] = None
+    children: dict['Directory'] = field(default_factory=lambda: dict())
+    file_size: int = 0
 
-answer_to_submit = None
+    @property
+    def size(self):
+        return self.file_size + sum([child.size for child in self.children.values()])
+
+dirs = [Directory()]
+
+for cmd in input_data:
+    match re.match(r"\$ cd (?P<cd>.*)|\$ ls|dir (?P<dir>.*)|(?P<size>\d+) .*", cmd).groupdict():
+        case {"cd": '/'}:
+            cwd = dirs[0]
+        case {"cd": '..'}:
+            cwd = cwd.parent
+        case {"cd": dir_name} if dir_name:
+            cwd = cwd.children[dir_name]
+        case {"dir": dir_list} if dir_list:
+            cwd.children[dir_list] = Directory(parent=cwd)
+            dirs.append(cwd.children[dir_list])
+        case {"size": size} if size:
+            cwd.file_size += int(size)
+
+answer_to_submit = min([d.size for d in dirs if d.size >= (dirs[0].size - 40_000_000)])
 ############################
 
 # submit answer
