e6bfc73614ac570f131ffc385211e7a4f318c8c9
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-09 13:15:17 -0600

    day8

--- src/d08/amabry_08A.py
+++ src/d08/amabry_08A.py
@@ -1,18 +1,44 @@
 from aocd import models
 from src.utils import parse_data
+import numpy as np
 
 # create puzzle
 puzzle = models.Puzzle(year=2022, day=8)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
+# input_data = parse_data(puzzle.example_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+trees = np.array([np.array([int(n) for n in row]) for row in input_data])
+vis = np.zeros(trees.shape, dtype=int)
 
-answer_to_submit = None
+def look_for_tree(tallest, x, y):
+    if trees[x, y] > tallest:
+        vis[x, y] = 1
+    return max(tallest, trees[x, y])
+
+
+for x in range(trees.shape[0]):
+    max_tree = -1
+    for y in range(trees.shape[1]):
+        max_tree = look_for_tree(max_tree, x, y)
+
+    max_tree = -1
+    for y in range(trees.shape[1] - 1, -1, -1):
+        max_tree = look_for_tree(max_tree, x, y)
+
+for y in range(trees.shape[1]):
+    max_tree = -1
+    for x in range(trees.shape[0]):
+        max_tree = look_for_tree(max_tree, x, y)
+
+    max_tree = -1
+    for x in range(trees.shape[0] - 1, -1, -1):
+        max_tree = look_for_tree(max_tree, x, y)
+
+answer_to_submit = vis.sum()
 ############################
 
 # submit answer
--- src/d08/amabry_08B.py
+++ src/d08/amabry_08B.py
@@ -1,18 +1,41 @@
+from itertools import takewhile
+
 from aocd import models
 from src.utils import parse_data
+import numpy as np
 
 # create puzzle
 puzzle = models.Puzzle(year=2022, day=8)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
+# input_data = parse_data(puzzle.example_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+trees = np.array([np.array([int(n) for n in row]) for row in input_data])
+vis = np.zeros(trees.shape, dtype=int)
 
-answer_to_submit = None
+def count_trees(h, view):
+    seen = 0
+    for tree in view:
+        seen += 1
+        if tree >= h:
+            break
+
+    return seen
+
+def count_all_trees():
+    for y in range(trees.shape[1]):
+        for x in range(trees.shape[0]):
+            vis[y, x] = count_trees(trees[y, x], np.flip(trees[:y, x]))
+            vis[y, x] *= count_trees(trees[y, x], trees[y+1:, x])
+            vis[y, x] *= count_trees(trees[y, x], np.flip(trees[y, :x]).flatten())
+            vis[y, x] *= count_trees(trees[y, x], trees[y, x+1:].flatten())
+
+count_all_trees()
+
+answer_to_submit = np.max(vis)
 ############################
 
 # submit answer
