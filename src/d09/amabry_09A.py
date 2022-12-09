48cd490290565df341c3fcdc096c13da7d1de81e
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-09 14:58:36 -0600

    day9

--- src/d09/amabry_09A.py
+++ src/d09/amabry_09A.py
@@ -1,3 +1,4 @@
+import numpy as np
 from aocd import models
 from src.utils import parse_data
 
@@ -5,14 +6,22 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=9)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+delta = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
+moves = [np.array(delta[x[0]], dtype=int) for x in input_data for _ in range(int(x[2:]))]
+T = np.zeros(2, dtype=int)
+positions = set()
 
-answer_to_submit = None
+for h in np.cumsum(moves, axis=0):
+    diff = h - T
+    if np.max(np.abs(diff)) >= 2:
+        T += np.sign(diff)
+    positions.add(tuple(T))
+
+answer_to_submit = len(positions)
 ############################
 
 # submit answer
--- src/d09/amabry_09B.py
+++ src/d09/amabry_09B.py
@@ -1,3 +1,4 @@
+import numpy as np
 from aocd import models
 from src.utils import parse_data
 
@@ -5,14 +6,23 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=9)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+delta = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
+moves = [np.array(delta[x[0]], dtype=int) for x in input_data for _ in range(int(x[2:]))]
+tails = np.zeros((9, 2), dtype=int)
+positions = set()
 
-answer_to_submit = None
+for h in np.cumsum(moves, axis=0):
+    for i in range(9):
+        diff = (h if not i else tails[i - 1]) - tails[i]
+        if np.max(np.abs(diff)) >= 2:
+            tails[i] += np.sign(diff)
+    positions.add(tuple(tails[8]))
+
+answer_to_submit = len(positions)
 ############################
 
 # submit answer
