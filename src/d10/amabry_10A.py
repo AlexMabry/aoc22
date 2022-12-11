6fca2a58bf16c12efb1ac46674f80f3a6fefcf50
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-11 10:11:03 -0600

    day10

--- src/d10/amabry_10A.py
+++ src/d10/amabry_10A.py
@@ -1,3 +1,6 @@
+import re
+
+import numpy as np
 from aocd import models
 from src.utils import parse_data
 
@@ -5,14 +8,22 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=10)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+register = np.array([0, 1], dtype=int)
 
-answer_to_submit = None
+for cmd in input_data:
+    match re.match(r"(?P<noop>noop).*|addx (?P<addx>[-0-9]+)", cmd).groupdict():
+        case {"noop": noop} if noop:
+            register = np.append(register, [0])
+        case {"addx": addx}:
+            register = np.append(register, [int(addx), 0])
+
+values = np.cumsum(register) * np.arange(1, len(register)+1)
+
+answer_to_submit = int(sum([values[r] for r in range(19, len(values), 40)]))
 ############################
 
 # submit answer
--- src/d10/amabry_10B.py
+++ src/d10/amabry_10B.py
@@ -1,3 +1,6 @@
+import re
+
+import numpy as np
 from aocd import models
 from src.utils import parse_data
 
@@ -5,12 +8,24 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=10)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+register = np.array([0, 1], dtype=int)
+
+for cmd in input_data:
+    match re.match(r"(?P<noop>noop).*|addx (?P<addx>[-0-9]+)", cmd).groupdict():
+        case {"noop": noop} if noop:
+            register = np.append(register, [0])
+        case {"addx": addx}:
+            register = np.append(register, [int(addx), 0])
+
+for cycle, val in enumerate(np.cumsum(register)):
+    pos = cycle % 40
+    print("###" if pos-1 <= val <= pos+1 else "   ", end="")
+    if pos == 39:
+        print()
 
 answer_to_submit = None
 ############################
