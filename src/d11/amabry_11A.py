63f62daabfa957e3ceff02cd142f871377d50ee9
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-12 08:41:30 -0600

    day11

--- src/d11/amabry_11A.py
+++ src/d11/amabry_11A.py
@@ -1,19 +1,46 @@
+from dataclasses import dataclass
+from math import prod
+
 from aocd import models
-from src.utils import parse_data
 
 # create puzzle
 puzzle = models.Puzzle(year=2022, day=11)
 
-# format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
-
 ############################
-# Solve puzzle
-print(input_data)
+@dataclass
+class Monkey:
+    items: list[int]
+    operation: lambda x: int
+    divisible: int
+    target: dict[bool, int]
+    count: int = 0
 
-answer_to_submit = None
+    def test(self) -> tuple[int, int]:
+        self.count += 1
+        inspected = self.operation(self.items.pop(0)) // 3
+        return self.target[not inspected % self.divisible], inspected
+
+monkeys = {
+    0: Monkey([50, 70, 54, 83, 52, 78], lambda x: x * 3, 11, {True: 2, False: 7}),
+    1: Monkey([71, 52, 58, 60, 71], lambda x: x * x, 7, {True: 0, False: 2}),
+    2: Monkey([66, 56, 56, 94, 60, 86, 73], lambda x: x + 1, 3, {True: 7, False: 5}),
+    3: Monkey([83, 99], lambda x: x + 8, 5, {True: 6, False: 4}),
+    4: Monkey([98, 98, 79], lambda x: x + 3, 17, {True: 1, False: 0}),
+    5: Monkey([76], lambda x: x + 4, 13, {True: 6, False: 3}),
+    6: Monkey([52, 51, 84, 54], lambda x: x * 17, 19, {True: 4, False: 1}),
+    7: Monkey([82, 86, 91, 79, 94, 92, 59, 94], lambda x: x + 7, 2, {True: 5, False: 3}),
+}
+
+for r in range(20):
+    for m in monkeys.values():
+        while m.items:
+            monkey, item = m.test()
+            monkeys[monkey].items.append(item)
+
+answer_to_submit = prod(sorted([m.count for m in monkeys.values()])[-2:])
 ############################
+
+print(answer_to_submit)
 
 # submit answer
 puzzle.answer_a = answer_to_submit
