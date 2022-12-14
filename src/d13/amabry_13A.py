b44d9539648c6274501f3e86820fceb1b6e24573
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-14 09:30:43 -0600

    day13

--- src/d13/amabry_13A.py
+++ src/d13/amabry_13A.py
@@ -5,14 +5,29 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=13)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
+# input_data = parse_data(puzzle.example_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+p_iter = iter([eval(packet) for packet in input_data if packet])
 
-answer_to_submit = None
+def compare(l, r):
+    if (llist := isinstance(l, list)) != (rlist := isinstance(r, list)):
+        return compare(l if llist else [l], r if rlist else [r])
+    elif llist:
+        while l and r:
+            if (res := compare(l[0], r[0])) != 0:
+                return res
+            l, r = l[1:], r[1:]
+
+        return 0 if not l and not r else -1 if not l else 1
+    else:
+        return -1 if l < r else 1 if l > r else 0
+
+results = [compare(left, right) for left, right in zip(p_iter, p_iter)]
+
+answer_to_submit = sum([pos+1 for pos, result in enumerate(results) if result == -1])
 ############################
 
 # submit answer
--- src/d13/amabry_13B.py
+++ src/d13/amabry_13B.py
@@ -1,18 +1,35 @@
 from aocd import models
 from src.utils import parse_data
+from functools import cmp_to_key
 
 # create puzzle
 puzzle = models.Puzzle(year=2022, day=13)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+packets = [eval(packet) for packet in input_data if packet]
+packets.append([[2]])
+packets.append([[6]])
 
-answer_to_submit = None
+def compare(l, r):
+    if (llist := isinstance(l, list)) != (rlist := isinstance(r, list)):
+        return compare(l if llist else [l], r if rlist else [r])
+    elif llist:
+        while l and r:
+            if (res := compare(l[0], r[0])) != 0:
+                return res
+            l, r = l[1:], r[1:]
+
+        return 0 if not l and not r else -1 if not l else 1
+    else:
+        return -1 if l < r else 1 if l > r else 0
+
+ordered = list(sorted(packets, key=cmp_to_key(compare)))
+
+answer_to_submit = (ordered.index([[2]])+1) * (ordered.index([[6]])+1)
 ############################
 
 # submit answer
