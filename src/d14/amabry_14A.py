e16f1f2d3dd904b7522bb7e10c09b2ab04f5ff60
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-14 13:37:34 -0600

    day14

--- src/d14/amabry_14A.py
+++ src/d14/amabry_14A.py
@@ -1,3 +1,5 @@
+from itertools import pairwise, product
+
 from aocd import models
 from src.utils import parse_data
 
@@ -5,14 +7,30 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=14)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+endpoints = [[tuple(int(n) for n in pair.split(',')) for pair in row.split(' -> ')] for row in input_data]
+paths = [pair for points in endpoints for pair in pairwise(points)]
+rocks = {pd for path in paths for pd in product(*[{n for n in range(min(p), max(p) + 1)} for p in zip(*path)])}
+max_depth = max(r[1] for r in rocks)
+sand = set()
 
-answer_to_submit = None
+def move_grain(g):
+    moves = [(g[0], g[1]+1), (g[0]-1, g[1]+1), (g[0]+1, g[1]+1)]
+    return next((m for m in moves if g[1] < max_depth and m not in sand and m not in rocks), None)
+
+grain = (500, 0)
+while grain[1] < max_depth:
+    while new_pos := move_grain(grain):
+        grain = new_pos
+
+    if grain[1] < max_depth:
+        sand.add(grain)
+        grain = (500, 0)
+
+answer_to_submit = len(sand)
 ############################
 
 # submit answer
