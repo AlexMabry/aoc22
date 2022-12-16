12b3ae2a547417294c82efdf08bd4116fc820478
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-16 08:33:17 -0600

    day15

--- src/d15/amabry_15A.py
+++ src/d15/amabry_15A.py
@@ -1,3 +1,7 @@
+import re
+
+import numpy as np
+from scipy.spatial.distance import cityblock
 from aocd import models
 from src.utils import parse_data
 
@@ -5,14 +9,22 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=15)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
+# input_data = parse_data(puzzle.example_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+pattern = re.compile(r'Sensor at x=(?P<x1>[-\d]+), y=(?P<y1>[-\d]+): closest beacon is at x=(?P<x2>[-\d]+), y=(?P<y2>[-\d]+)')
+matches = [pattern.match(cmd).groupdict() for cmd in input_data]
+sensors = [np.array([int(m['y1']), int(m['x1'])]) for m in matches]
+beacons = [np.array([int(m['y2']), int(m['x2'])]) for m in matches]
+distances = [cityblock(s, b) for s, b in zip(sensors, beacons)]
 
-answer_to_submit = None
+LINE = 2_000_000
+close_sensors = [range(s[1] - w, s[1] + w + 1) for s, d in zip(sensors, distances) if (w := d - abs(s[0]-LINE)) >= 0]
+taken = {x for sensor_range in close_sensors for x in sensor_range}
+
+answer_to_submit = len(taken - {x for [y, x] in beacons if y == LINE})
 ############################
 
 # submit answer
--- src/d15/amabry_15B.py
+++ src/d15/amabry_15B.py
@@ -1,3 +1,8 @@
+import re
+from typing import Optional, Iterable
+
+import numpy as np
+from scipy.spatial.distance import cityblock
 from aocd import models
 from src.utils import parse_data
 
@@ -5,14 +10,38 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=15)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
+# input_data = parse_data(puzzle.example_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+pattern = re.compile(r'Sensor at x=(?P<x1>[-\d]+), y=(?P<y1>[-\d]+): closest beacon is at x=(?P<x2>[-\d]+), y=(?P<y2>[-\d]+)')
+matches = [pattern.match(cmd).groupdict() for cmd in input_data]
+sensors = [np.array([int(m['y1']), int(m['x1'])]) for m in matches]
+beacons = [np.array([int(m['y2']), int(m['x2'])]) for m in matches]
+distances = [cityblock(s, b) for s, b in zip(sensors, beacons)]
+max_line = 4000000
 
-answer_to_submit = None
+def whats_missing(line) -> Optional[int]:
+    merged = []
+    for r in sorted(((max(0, s[1] - w), min(s[1] + w, max_line)) for s, d in zip(sensors, distances) if
+     (w := d - abs(s[0] - line)) >= 0)):
+        if not merged:
+            merged.append(r)
+        else:
+            prev_start, prev_end = merged[-1]
+            curr_start, curr_end = r
+            if prev_end >= curr_start - 1:
+                merged[-1] = (prev_start, max(prev_end, curr_end))
+            else:
+                merged.append(r)
+
+        if len(merged) == 1 and merged[0][1] - merged[0][0] == max_line:
+            return None
+
+    return merged[1][0]-1
+
+answer_to_submit = next(missing*max_line+line for line in range(max_line) if (missing := whats_missing(line)) is not None)
 ############################
 
 # submit answer
