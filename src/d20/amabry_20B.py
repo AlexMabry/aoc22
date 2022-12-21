bb7cb17bc5be04c49b9918b405c7f03723a9059f
Alex Mabry <alex.mabry@synapse-wireless.com>
2022-12-20 21:23:23 -0600

    day20

--- src/d20/amabry_20B.py
+++ src/d20/amabry_20B.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from itertools import pairwise
+
 from aocd import models
 from src.utils import parse_data
 
@@ -5,14 +8,61 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2022, day=20)
 
 # format data
-# input_data = parse_data(puzzle.input_data)
-input_data = parse_data(puzzle.example_data)
+input_data = parse_data(puzzle.input_data)
 
 ############################
 # Solve puzzle
-print(input_data)
+@dataclass
+class LinkedNode:
+    value : int
+    next : "LinkedNode" = None
+    prev : "LinkedNode" = None
 
-answer_to_submit = None
+class CircularLinkedList:
+    def __init__(self, values):
+        self.nodes = {ix: LinkedNode(x) for ix, x in enumerate(values)}
+        self.length = len(values)
+
+        for a, b in pairwise([*range(len(values)), 0]):
+            self.nodes[a].next = self.nodes[b]
+            self.nodes[b].prev = self.nodes[a]
+
+    def mix(self):
+        for n in range(self.length):
+            node = self.nodes[n]
+
+            # remove node
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+            # find destination
+            after = node.prev
+            for _ in range(abs(node.value) % (self.length - 1)):
+                after = after.next if node.value > 0 else after.prev
+
+            # insert node
+            node.next = after.next
+            node.prev = after
+            after.next.prev = node
+            after.next = node
+
+    def value_after_zero(self, places):
+        # find node with value 0
+        node = self.nodes[0]
+        while node.value != 0:
+            node = node.next
+
+        for _ in range(places % self.length):
+            node = node.next
+        return node.value
+
+decryption_key = 811_589_153
+clist = CircularLinkedList([int(n)*decryption_key for n in input_data])
+
+for _ in range(10):
+    clist.mix()
+
+answer_to_submit = sum(clist.value_after_zero(n*1000) for n in range(1, 4))
 ############################
 
 # submit answer
